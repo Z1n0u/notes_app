23e00bbfd6002bc87e8abec6b9a8ada68fb37311
Z1n0u <taztala1@gmail.com>
2023-02-15 15:12:04 +0100

    create Notes app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,47 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NotesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Myhomepage(),
-    );
-  }
-}
-
-class Myhomepage extends StatelessWidget {
-  Myhomepage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFF5FF67),
-        centerTitle: true,
-        title: Text(
-          "الأربعاء 2 فيفري 2023",
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: Card(
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(color: Colors.black),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Container(
-          padding: EdgeInsets.all(25),
-          child: const Text(
-            'حل تد أرشي',
-            style: TextStyle(fontSize: 25),
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
