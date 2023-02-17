0d63dc30f8536f7076ab848b9301d29045edb126
Z1n0u <taztala1@gmail.com>
2023-02-17 22:19:28 +0100

    created notes Model

---
+++ app/lib/Models/Note_model.dart
@@ -0,0 +1,12 @@
+class Notesmodel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  Notesmodel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
